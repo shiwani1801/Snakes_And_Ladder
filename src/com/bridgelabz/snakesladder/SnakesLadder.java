1fabd1fb3bbdd1a2aba6ae50bb8c1ea94b7ad45c
shiwani <shiwanipaunikar1@gmail.com>
2022-11-24 07:40:44 -0800

    [shiwani]snakes and ladder program

---
+++ src/com/bridgelabz/snakesladder/SnakesLadder.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.snakesladder;
+
+public class SnakesLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to snakes and ladder");
+    }
+}
