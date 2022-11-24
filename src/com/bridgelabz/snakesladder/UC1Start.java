04c42ce82f959dd546ff8bed79904818d49cab65
shiwani <shiwanipaunikar1@gmail.com>
2022-11-24 08:34:27 -0800

    [shiwani]Player position zero program is added

---
+++ src/com/bridgelabz/snakesladder/UC1Start.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.snakesladder;
+
+public class UC1Start {
+    public static void main(String[] args) {
+        System.out.println("Welcome to snake simulator program ");
+        int playerPosition=0;
+
+        System.out.println("currunt position is:"+playerPosition);
+
+    }
+}
